ec5ebda6e627531dcc2cbfbf076a3315b31f2426
Mike Ochs <42348035+mikefactorial@users.noreply.github.com>
2022-06-30 13:26:48 -0400

    Add project files.

---
+++ LocalTestApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingWorkflowActivity;
+
+namespace LocalTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // e.g. https://yourorg.crm.dynamics.com
+            string url = "https://yourorg.crm.dynamics.com";
+
+            // e.g. you@yourorg.onmicrosoft.com
+            string userName = "you@yourorg.onmicrosoft.com";
+
+            // e.g. y0urp455w0rd 
+            string password = "y0urp455w0rd";
+
+            string conn = $@"
+                Url = {url};
+                AuthType = OAuth;
+                UserName = {userName};
+                Password = {password};
+                AppId = 51f81489-12ee-4a9e-aaae-a2591f45987d;
+                RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
+                LoginPrompt=Auto;
+                RequireNewInstance = True";
+
+            MyWorkflowActivity activity = new MyWorkflowActivity();
+            DummyTracingService trace = new DummyTracingService();
+            using (var svc = new CrmServiceClient(conn))
+            {
+                QueryExpression q = new QueryExpression("account");
+                var allAccounts = svc.RetrieveMultiple(q);
+                foreach (var account in allAccounts.Entities)
+                {
+                    activity.MyWorkflowLogic(svc, trace, account);
+                }
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TestingWorkflowActivity/MyWorflowActivity.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestingWorkflowActivity
+{
+    public class MyWorkflowActivity : CodeActivity
+    {
+
+        //In your case you probably have an input parameter that is the timesheet / weekly timesheet using account as an example
+        [Input("UserAccount")]  
+        public InArgument<Entity> UserAccount
+        {  
+            get;  
+            set;  
+        }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            // Obtain the Organization Service factory service from the service provider
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)context.GetExtension<IOrganizationServiceFactory>();
+            IWorkflowContext worfklowContext = context.GetExtension<IWorkflowContext>();
+            IOrganizationService service = factory.CreateOrganizationService(worfklowContext.UserId);
+            ITracingService tracer = context.GetExtension<ITracingService>();
+
+            this.MyWorkflowLogic(service, tracer, UserAccount.Get(context));
+        }
+
+
+        public void MyWorkflowLogic(IOrganizationService organizationService, ITracingService tracer, Entity account)
+        {
+            //I can call this method from a console application and just supply the parameters it expects and run it locally.
+            //You'd move all of your logic out of the Execute above and put it here. Here I'm retrieving contacts associated with the account as an example 
+            QueryExpression q = new QueryExpression("contact");
+            FilterExpression f = new FilterExpression();
+            f.AddCondition("parentcustomerid", ConditionOperator.Equal, account["accountid"]);
+            q.Criteria = f;
+            var contactsForAccount = organizationService.RetrieveMultiple(q);
+
+            tracer.Trace("Retrieved {0} contact(s) for account {1}", contactsForAccount.Entities.Count, account.GetAttributeValue<string>("name"));
+            foreach (var contact in contactsForAccount.Entities)
+            {
+                tracer.Trace("Retrieved contact {0}", contact.GetAttributeValue<string>("fullname"));
+            }
+        }
+    }
+}
